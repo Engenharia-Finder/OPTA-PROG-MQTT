d6697cb6c088d04a591ae066b99d1782b2f7a7c2
Engenharia-Finder <133161771+Engenharia-Finder@users.noreply.github.com>
2023-05-10 17:01:32 -0300

    Add files via upload

---
+++ OPTA-MQTT.ino
@@ -0,0 +1,119 @@
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32) || defined(ARDUINO_GIGA) || defined(ARDUINO_OPTA)
+  #include <WiFi.h>
+#endif 
+#include <PubSubClient.h>
+
+
+
+//WiFi
+const char* SSID = "Celular daniel";                // SSID / nome da rede WiFi que deseja se conectar
+const char* PASSWORD = "diego1993";   // Senha da rede WiFi que deseja se conectar
+WiFiClient wifiClient;                        
+ 
+//MQTT Server
+const char* BROKER_MQTT = "broker.emqx.io"; //URL do broker MQTT que se deseja utilizar
+int BROKER_PORT = 1883;                      // Porta do Broker MQTT
+
+#define ID_MQTT  "mqttx_4cc86222"             //Informe um ID unico e seu. Caso sejam usados IDs repetidos a ultima conexão irá sobrepor a anterior. 
+#define TOPIC_SUBSCRIBE "arduino/simple"   //Informe um Tópico único. Caso sejam usados tópicos em duplicidade, o último irá eliminar o anterior.
+PubSubClient MQTT(wifiClient);        // Instancia o Cliente MQTT passando o objeto espClient
+
+//Declaração das Funções
+void mantemConexoes();  //Garante que as conexoes com WiFi e MQTT Broker se mantenham ativas
+void conectaWiFi();     //Faz conexão com WiFi
+void conectaMQTT();     //Faz conexão com Broker MQTT
+void recebePacote(char* topic, byte* payload, unsigned int length);
+
+void setup() {
+  pinMode(LED_D0, OUTPUT);  
+  pinMode(D3,OUTPUT);
+
+  Serial.begin(115200);
+
+  conectaWiFi();
+  MQTT.setServer(BROKER_MQTT, BROKER_PORT);  
+  MQTT.setCallback(recebePacote); 
+}
+
+void loop() {
+  mantemConexoes();
+  MQTT.loop();
+}
+
+void mantemConexoes() {
+    if (!MQTT.connected()) {
+       conectaMQTT(); 
+    }
+    
+    conectaWiFi(); //se não há conexão com o WiFI, a conexão é refeita
+}
+
+void conectaWiFi() {
+
+  if (WiFi.status() == WL_CONNECTED) {
+     return;
+  }
+        
+  Serial.print("Conectando-se na rede: ");
+  Serial.print(SSID);
+  Serial.println("  Aguarde!");
+
+  WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI  
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+      Serial.print(".");
+  }
+  
+  Serial.println();
+  Serial.print("Conectado com sucesso, na rede: ");
+  Serial.print(SSID);  
+  Serial.print("  IP obtido: ");
+  Serial.println(WiFi.localIP());
+  
+}
+
+void conectaMQTT() { 
+    while (!MQTT.connected()) {
+        Serial.print("Conectando ao Broker MQTT: ");
+        Serial.println(BROKER_MQTT);
+        if (MQTT.connect(ID_MQTT)) {
+            Serial.println("Conectado ao Broker com sucesso!");
+            MQTT.subscribe(TOPIC_SUBSCRIBE);
+        } 
+        else {
+            Serial.println("Noo foi possivel se conectar ao broker.");
+            Serial.println("Nova tentatica de conexao em 10s");
+            delay(10000);
+        }
+    }
+}
+
+void recebePacote(char* topic, byte* payload, unsigned int length) 
+{
+    String msg;
+
+    //obtem a string do payload recebido
+    for(int i = 0; i < length; i++) 
+    {
+       char c = (char)payload[i];
+       msg += c;
+    }
+
+    if (msg == "0") {
+       digitalWrite(LED_D0, LOW);
+       digitalWrite(D3,HIGH);
+    }
+
+    if (msg == "1") {
+       digitalWrite(LED_D0, HIGH);
+       digitalWrite(D3,LOW);
+    }
+    
+}
